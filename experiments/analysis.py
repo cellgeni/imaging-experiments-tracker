4ab04ee25a03cfdb46ef7e8f5123561b0bad4b37
Anton Khodak <ak27@sanger.ac.uk>
2020-04-23 12:24:51 +0100

    Delete analysis models until they are needed

--- experiments/analysis.py
+++
@@ -1,9 +0,0 @@
-from experiments.models import Analysis, Status
-
-
-def submit_analysis(analysis: Analysis):
-    """
-    Handles analysis request submission
-    """
-    analysis.status = Status.STARTED
-    analysis.save()
