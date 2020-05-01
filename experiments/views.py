cb26d06dac5f39f10ac4719bce38958d70e87676
prete <martin.prete@sanger.ac.uk>
2020-05-01 14:02:10 +0100

    Make PEP8 happy

--- experiments/views.py
+++ experiments/views.py
@@ -42,7 +42,8 @@ class XLSImportView(View):
     def post(self, request, *args, **kwargs):
         form = XLSUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            log = self.dump_file_on_disk_import_it_and_get_import_log(request.FILES['file'])
+            log = self.dump_file_on_disk_import_it_and_get_import_log(
+                request.FILES['file'])
         else:
             log = ["Invalid submission"]
         return render(request, self.template_name, {'form': form,
@@ -133,16 +134,24 @@ class UUIDAndCreateModeInjectorView(XLSImportView, ExcelDownloadView):
 
 class DataView(View):
     """
-    View to serve embeded metabase question
+    View to serve embedded metabase question
     """
 
     def get(self, request, *args, **kwargs):
         payload = {
-            "resource": { "question": 1 },
-            "params": { "authorized_projects": None },
-            "exp": int((datetime.datetime.now() + datetime.timedelta(minutes = 10)).timestamp()) * 1000 # 10 minute expiration in miliseconds
+            "resource": {"question": 1},
+            "params": {"authorized_projects": None},
+            # 10 minute expiration in miliseconds
+            "exp": int((
+                datetime.datetime.now() +
+                datetime.timedelta(minutes=10)
+            ).timestamp()) * 1000
         }
 
-        token = jwt.encode(payload, settings.METABASE_SECRET_KEY, algorithm="HS256")
-        iframeUrl = f"{settings.METABASE_SITE_URL}/embed/question/{token.decode('UTF-8')}"
-        return render(request, 'dataview.html', {'iframeUrl': iframeUrl})
+        token = jwt.encode(
+            payload, settings.METABASE_SECRET_KEY, algorithm="HS256")
+
+        iframe_url = "{url}/embed/question/{token}".format(
+            url=settings.METABASE_SITE_URL, token=tokentoken.decode('UTF-8'))
+
+        return render(request, 'dataview.html', {'iframeUrl': iframe_url})
