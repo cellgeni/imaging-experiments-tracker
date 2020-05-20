c41a0698db24cc6732a2cb00e163c72c7796bd2a
prete <martin.prete@sanger.ac.uk>
2020-05-20 13:53:18 +0100

    Add log parser, update XLSProcessView and template

--- experiments/xls/stream_logging.py
+++ experiments/xls/stream_logging.py
@@ -12,6 +12,8 @@ class StreamLogging:
     def __init__(self):
         self._stream = io.StringIO()
         self.handler = logging.StreamHandler(self._stream)
+        self.handler.setFormatter(
+            logging.Formatter("%(levelname)s::%(message)s"))
 
     def __enter__(self) -> 'StreamLogging':
         xls_logger.addHandler(self.handler)
@@ -22,3 +24,42 @@ class StreamLogging:
 
     def get_log(self) -> str:
         return self._stream.getvalue()
+
+
+class LogParser(object):
+    """
+    A parser that converts raw logs into dicts for ease of use
+    """
+
+    def __init__(self, raw_logs=[]):
+        self.raw_logs = raw_logs
+        self.logs = []
+        self.parse_logs(self.raw_logs)
+
+    def parse_logs(self, raw_logs):
+        for log in raw_logs:
+            self.parse(log)
+
+    def parse(self, raw_log):
+        if not raw_log:
+            return
+        log_tuple = raw_log.split('::')
+        if len(log_tuple) == 2:
+            self.add(log_tuple[0], log_tuple[1])
+        else:
+            self.add_error(raw_log)
+
+    def add(self, level, message):
+        self.logs.append({"level": level, "message": message})
+
+    def add_error(self, message):
+        self.logs.append({"level": "ERROR", "message": message})
+
+    def get_logs(self):
+        return self.logs
+
+    def get_raw_logs(self):
+        return self.raw_logs
+
+    def get_error_count(self):
+        return len([l for l in self.logs if l['level'] != "INFO"])
