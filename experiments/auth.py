fbcd5cd85cf6039b6b68deafa564ed4ef6cc5beb
Anton Khodak <ak27@sanger.ac.uk>
2020-06-01 11:01:38 +0100

    Add adding and removing roles

--- experiments/auth.py
+++ experiments/auth.py
@@ -1,88 +1,106 @@
 import os
-from typing import List, Union
-from django.db import connections
-from django.conf import settings
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
+from typing import Dict, List, Union
 
-from casbin_sqlalchemy_adapter import Adapter
 import casbin
-
 import yaml
-# def get_obj_id_from_name(model: str):
-#     """Get id of object by type and name"""
-#     ct = ContentType.objects.get(model=model)
-#     obj_model = ct.get_object_for_this_type(name=model.id)
+from casbin_sqlalchemy_adapter import Adapter
+from django.conf import settings
+from django.db import connections
+
+RoleDefinitionT = Dict[str, List[str]]
+
+
+class CasbinEnforcerFactory:
+
+    @staticmethod
+    def _create_connection_string():
+        """Create a connection string for a database connection."""
+        connection_params = connections['default'].get_connection_params()
+        connection_string = "{vendor}://{user}:{password}@{host}:{port}/{database}"
+        return connection_string.format(
+            vendor=connections['default'].vendor,
+            user=connection_params['user'],
+            password=connection_params['password'],
+            host=connection_params['host'],
+            port=connection_params.get('port', 5432),
+            database=connection_params['database'])
+
+    @classmethod
+    def _create_adapter(cls):
+        """Create database adapter for casbin enforcer."""
+        if connections['default'].vendor == "sqlite":
+            connection_string = "sqlite://"
+        else:
+            connection_string = cls._create_connection_string()
+        return Adapter(connection_string)
+
+    @classmethod
+    def create_enforcer(cls):
+        """Return an instance of casbin enforcer."""
+        adapter = cls._create_adapter()
+        model_path = os.path.join(settings.CASBIN_ROOT, 'model.conf')
+        return casbin.Enforcer(model_path, adapter, False)
 
 
 class Authorization:
     """Authorization helper class that wraps casbin access control"""
-    # private class vars used to connect the enfonrcer to the backend database
-    __connection_params = connections['default'].get_connection_params()
-    __connection_string = "{vendor}://{user}:{password}@{host}:{port}/{database}"
-    __adapter = Adapter(__connection_string.format(
-        vendor=connections['default'].vendor,
-        user=__connection_params['user'],
-        password=__connection_params['password'],
-        host=__connection_params['host'],
-        port=__connection_params.get('port', 5432),
-        database=__connection_params['database']))
-    _enforcer = casbin.Enforcer(os.path.join(
-        settings.CASBIN_ROOT, 'model.conf'), __adapter, False)
+    _enforcer = CasbinEnforcerFactory.create_enforcer()
 
     def __init__(self, roles_filename: str) -> None:
         with open(roles_filename) as f:
-            self.roles = yaml.load(f)
+            self.roles = yaml.safe_load(f)
 
-    @staticmethod
-    def add_permission(user_id: int, project_id: int, permission: str) -> None:
-        """Add new permission to casbin rules"""
-        Authorization._enforcer.add_permission_for_user(
+    def add_permission(self, user_id: int, project_id: int, permission: str) -> None:
+        """Add new permission to casbin rules."""
+        self._enforcer.add_permission_for_user(
             str(user_id), str(project_id), str(project_id), permission)
 
-    @staticmethod
-    def remove_permission(user_id: str, project_id: str,  permission: str) -> None:
-        """Delete permission from casbin rules"""
-        Authorization._enforcer.delete_permission_for_user(
+    def remove_permission(self, user_id: str, project_id: str,  permission: str) -> None:
+        """Delete permission from casbin rules."""
+        self._enforcer.delete_permission_for_user(
             str(user_id), str(project_id), str(project_id), permission)
+
+    def _attach_role_definition(self, user_id: int, project_id: int, role_definition: RoleDefinitionT, role_name: str) -> None:
+        """Add a role and permissions from a role definition to a user in a given project."""
+        self.remove_existing_role(user_id, project_id)
+        self._add_permissions_from_a_role_definition(role_definition, user_id, project_id)
+        self._enforcer.add_role_for_user_in_domain(str(user_id), role_name, str(project_id))
+
+    def _add_permissions_from_a_role_definition(self, role_definition: RoleDefinitionT, user_id: int, project_id: int) -> None:
+        for permission in role_definition['permissions']:
+            self.add_permission(user_id, project_id, permission)
+
+    def remove_existing_role(self, user_id: int, project_id: int) -> None:
+        """Remove an existing role if any from a user for a project."""
+        current_role = self.get_role(user_id, project_id)
+        if current_role:
+            self.remove_role(user_id, project_id, current_role)
 
     def add_role(self, user_id: int, project_id: int, role_name: str) -> None:
-        """Add a role for a user in a given project."""
+        """Find a role in the list of roles and give it to a user in a given project."""
         for role in self.roles['roles']:
             if role['name'] == role_name:
-                for permission in role['permissions']:
-                    self.add_permission(user_id, project_id, permission)
-                self._enforcer.add_role_for_user_in_domain(
-                    str(user_id), role_name, str(project_id))
+                return self._attach_role_definition(user_id, project_id, role, role_name)
+        raise ValueError("Unknown role")
 
     def get_role(self, user_id: int, project_id: int) -> Union[str, None]:
         """Return a role for a user in a given project."""
         roles = self._enforcer.get_roles_for_user_in_domain(
             str(user_id), str(project_id))
-        if roles:
-            if len(roles) > 1:
-                raise ImplementationError(
-                    "A user should not have more than one project in a domain")
-            else:
-                return roles[0]
-        return None
+        if len(roles) > 1:
+            raise AttributeError("A user should not have more than one role for a project")
+        elif roles:
+            return roles[0]
+        else:
+            return None
 
     def remove_role(self, user_id: int, project_id: int, role_name: str) -> None:
         """Remove a role for a user in a given project."""
         self._enforcer.delete_roles_for_user_in_domain(
             str(user_id), role_name, str(project_id))
+        for policy in self._enforcer.get_permissions_for_user_in_domain(str(user_id), str(project_id)):
+            self.remove_permission(policy[0], policy[1], policy[-1])
 
-    @staticmethod
-    def get_user_roles(username: str) -> List[str]:
-        """Get stored roles for a given user"""
-        return Authorization._enforcer.get_roles_for_user(username)
-
-    @staticmethod
-    def enforce(user_id: int, project_id: str, action: str) -> bool:
-        """Check if a user can do an action on an object (obj and model)."""
-        # if User.objects.get(username=username).is_superuser:
-        #     casbin.log.log_printf(
-        #         f"Request: {username}, {model}, {_id}, {action} ---> True ")
-        #     return True
-        return Authorization._enforcer.enforce(
-            str(user_id), str(project_id), str(project_id), action)
+    def check_permission(self, user_id: int, project_id: str, permission: str) -> bool:
+        """Check if a user has permission on an object."""
+        return self._enforcer.enforce(str(user_id), str(project_id), str(project_id), permission)
--- experiments/tests/test_authorization.py
+++ experiments/tests/test_authorization.py
@@ -6,35 +6,69 @@ from experiments.auth import Authorization
 from experiments.models import Project
 from django.contrib.auth.models import User
 
+DELETE_PERMISSION = "delete"
+VIEW_PERMISSION = "view"
+OWNER_ROLE = "owner"
+VIEWER_ROLE = "viewer"
 
 class AuthorisationTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username="some", password="123")
         self.project = Project.objects.create(name="project")
-        #self.auth = Authorization(os.path.join(settings.CASBIN_ROOT, "roles.yaml"))
+        self.auth = Authorization(os.path.join(settings.CASBIN_ROOT, "roles.yaml"))
 
     def test_enforce(self):
         """
-        Get a project, get a user, assign a permission, check the permission
-        was applied. Remove the permission, check user no longer has it.
+        Check a user has a permission manually added to them.
+        Check a user no longer has a permission removed from them.
         """
         permission = "delete"
-        Authorization.add_permission(self.user.id, self.project.id, permission)
-        self.assertTrue(Authorization.enforce(
+        self.auth.add_permission(self.user.id, self.project.id, permission)
+        self.assertTrue(self.auth.check_permission(
             self.user.id, self.project.id, permission))
-        Authorization.remove_permission(
+        self.auth.remove_permission(
             self.user.id, self.project.id, permission)
-        self.assertFalse(Authorization.enforce(
+        self.assertFalse(self.auth.check_permission(
             self.user.id, self.project.id, permission))
 
     def test_add_role(self):
-        """Add role to a user"""
-        auth = Authorization(os.path.join(settings.CASBIN_ROOT, "roles.yaml"))
-        role = "owner"
-        auth.add_role(self.user.id, self.project.id, role)
-        self.assertEqual(role, auth.get_role(
+        """Check if a user has a role added to them."""
+        role = OWNER_ROLE
+        self.auth.add_role(self.user.id, self.project.id, role)
+        self.assertEqual(role, self.auth.get_role(
             self.user.id, self.project.id))
-        auth.remove_role(self.user.id, self.project.id, role)
-        self.assertEqual(None, auth.get_role(
+        self.auth.remove_role(self.user.id, self.project.id, role)
+        self.assertEqual(None, self.auth.get_role(
             self.user.id, self.project.id))
+    
+    def test_permission_removed_with_roled_removed(self):
+        """
+        Check if permission is assigned if a role is assigned. 
+        Check if permission is removed if the role is removed.
+        """
+        role = OWNER_ROLE
+        self.auth.add_role(self.user.id, self.project.id, role)
+        self.assertTrue(self.auth.check_permission(
+            self.user.id, self.project.id, DELETE_PERMISSION))
+        self.auth.remove_role(self.user.id, self.project.id, role)
+        self.assertFalse(self.auth.check_permission(
+            self.user.id, self.project.id, DELETE_PERMISSION))
+    
+    def test_permission_removed_with_roled_reassigned(self):
+        """
+        Check if permission is assigned if a role is assigned. 
+        Check if permission is removed if the other role is assigned. 
+        """
+        role = OWNER_ROLE
+        self.auth.add_role(self.user.id, self.project.id, role)
+        self.assertTrue(self.auth.check_permission(
+            self.user.id, self.project.id, DELETE_PERMISSION))
+        self.assertTrue(self.auth.check_permission(
+            self.user.id, self.project.id, VIEW_PERMISSION))
+        new_role = VIEWER_ROLE
+        self.auth.add_role(self.user.id, self.project.id, new_role)
+        self.assertFalse(self.auth.check_permission(
+            self.user.id, self.project.id, DELETE_PERMISSION))
+        self.assertTrue(self.auth.check_permission(
+            self.user.id, self.project.id, VIEW_PERMISSION))
